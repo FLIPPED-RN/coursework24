78305b8f0552140903abf05d7f7058e299e16006
Nazarov Ruslan <rnazarov142@gmail.com>
2024-07-02 14:13:42 +0200

    coursework v1.1

---
+++ coursework24/DataBase/DatabaseContext.cs
@@ -0,0 +1,10 @@
+using System.IO;
+using Microsoft.Data.Sqlite;
+using Path = System.Windows.Shapes.Path;
+
+namespace coursework24.DataBase;
+
+public class DatabaseContext
+{
+    
+}
---
+++ coursework24/View/LoginWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace coursework24.View;
+
+public partial class LoginWindow : Window
+{
+    public LoginWindow()
+    {
+        InitializeComponent();
+    }
+}
